6967b5e07366d5429adaea80cdbf65657ad0f1b5
robo7660 <98061415+robo7660@users.noreply.github.com>
2022-02-08 20:40:01 -0500

    Add reverse button for joystick.
    When button is pushed on the joystick, reverse controls.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -208,9 +208,20 @@ public class Robot extends TimedRobot
         yeetMode = !yeetMode;
       }
       if (yeetMode) {
-        drive.arcadeDrive(joystick1.getLeftX() * 1.0f, -joystick1.getLeftY() * 1.0f, true);
-      } else {
-        drive.arcadeDrive(joystick1.getLeftX() * 0.75f, -joystick1.getLeftY() * 0.90f, true);
+        if(joystick2.getAButton()){
+          drive.arcadeDrive(joystick1.getLeftX() * 1.0f, joystick1.getLeftY() * 1.0f, false);
+        }
+        else{
+            drive.arcadeDrive(joystick1.getLeftX() * 1.0f, -joystick1.getLeftY() * 1.0f, false);
+        }
+      } 
+      else {
+        if(joystick2.getAButton()){
+          drive.arcadeDrive(joystick1.getLeftX() * 0.75f, joystick1.getLeftY() * 0.9f, false);
+        }
+        else{
+          drive.arcadeDrive(joystick1.getLeftX() * 0.75f, -joystick1.getLeftY() * 0.90f, false);
+        }
       }
       left1.set(ControlMode.PercentOutput, left2.getMotorOutputPercent());
       right1.set(ControlMode.PercentOutput, right2.getMotorOutputPercent());

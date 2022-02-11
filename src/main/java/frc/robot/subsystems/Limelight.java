e11381b7a00a4b5c856038ccba9165d11d0e3933
Sephdude <61553976+Sephdude@users.noreply.github.com>
2022-02-10 19:04:02 -0500

    Tests the limelight distance formula. should pass

---
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -0,0 +1,61 @@
+package frc.robot.subsystems;
+
+public class Limelight {
+    
+    // distanceToTarget:
+    //   Return the distance to the hub along the horizontal plane.
+    //   Known (measured) values:
+    //      mountHeight (h1) - height above floor that camera is mounted.
+    //      mountAngle (a1) - angle in radians above horizontal that limelight is pointed.
+    //      targetHeight (h2) - target height (height of hub)
+    //   Given the limelight provided 'ty' value:
+    //      offsetAngle (a2) - vertical offset from crosshair to target
+    //   https://docs.limelightvision.io/en/latest/cs_estimating_distance.html
+    //
+    
+    public static double distanceToTarget(double h1, double a1,
+        double h2, double a2) {
+    //List the variables that will be needed for the equation
+    //this method only will work on cameras that are fixed
+    //They are all done in CENTIMETERS
+
+    
+    //This is the height of the target
+    //h2 = 100;
+    
+    //this is the height of the camera above the floor;
+    //h1 = 134;
+
+        //This is the angle of the fixed camera
+    //    a1 = 90;
+
+    /*This is the variable for the angle to the target you are aiming at.
+    The limelight will determine this for you*/
+    //    a2 =70;
+    
+        
+    //start the equation to calculate distance d = (h2-h1) / tan(a1+a2).
+
+    //h2 - h1
+    //h is the variable for both of the heights after they are subtracted from each other
+    //this makes the equation d = h / tan(a1+a2)
+        double h = h2 - h1;
+
+    //a1 + a2
+    //a is the variable for both of the angles
+    //this makes the equation d = h / tan(a)
+    double a = a1 + a2;
+
+    //tan(a)
+    //tanA is the tangent of the variable a
+    //this will make the equation d = h / tanA
+    double tanA = Math.tan(a);
+
+    //h / tanA
+    //d is the distance to the target
+    //this will make the equation d
+    double d = h / tanA;
+    return d;
+    
+    }
+}

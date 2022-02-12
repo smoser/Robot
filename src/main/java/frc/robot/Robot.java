5acd12329dc141ed0985b01bf383f9074006484e
robo7660 <98061415+robo7660@users.noreply.github.com>
2022-02-12 11:25:46 -0500

    Created Drive subsystem and DriveRobot Command
    This is functionally tested to drive two of the wheels.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -27,7 +27,7 @@ public class Robot extends TimedRobot {
   public void robotInit() {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
-    m_robotContainer = new RobotContainer();
+    m_robotContainer = RobotContainer.getInstance();
   }
 
   /**
@@ -56,7 +56,7 @@ public class Robot extends TimedRobot {
   /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
   public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+    //m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
     // schedule the autonomous command (example)
     if (m_autonomousCommand != null) {
---
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -0,0 +1,42 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import java.util.function.DoubleSupplier;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotContainer;
+
+public class Drive extends SubsystemBase {
+  /** Creates a new Drive subsystem. */
+
+    private CANSparkMax leftFront = new CANSparkMax(1, MotorType.kBrushless);
+    private CANSparkMax leftBack = new CANSparkMax(2, MotorType.kBrushless);
+    private CANSparkMax rightFront = new CANSparkMax(3, MotorType.kBrushless);
+    private CANSparkMax rightBack = new CANSparkMax(4, MotorType.kBrushless);
+    private MotorControllerGroup leftGroup = new MotorControllerGroup(leftFront, leftBack);
+    private MotorControllerGroup rightGroup = new MotorControllerGroup(rightFront, rightBack);
+
+    private DifferentialDrive driveDifferential = new DifferentialDrive(leftGroup, rightGroup);
+
+  public void arcadeDrive(DoubleSupplier speed, DoubleSupplier rotation) {
+      driveDifferential.arcadeDrive(speed.getAsDouble(), rotation.getAsDouble());
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}

cf0ad861b01e351f15f8f3b0155b0fa689c7a51a
Sephdude <61553976+Sephdude@users.noreply.github.com>
2022-02-22 15:18:39 -0500

    Created methods for the limelight information

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -11,6 +11,8 @@ import frc.robot.commands.FrontIntake;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.Drive;
 import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Limelight;
+import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -27,6 +29,8 @@ public class RobotContainer {
   private static RobotContainer robotContainer = new RobotContainer();
   private final Drive m_drive = new Drive();
   private final Intake m_intake = new Intake();
+
+  private Limelight limelight;
 
   private XboxController controller1 = new XboxController(0);
   private XboxController controller2 = new XboxController(1);

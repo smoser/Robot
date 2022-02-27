27023671ca4d1ca10f55b0aa4e59d23c5d186306
Scott Moser <smoser@ubuntu.com>
2022-02-27 15:36:00 -0500

    Update Limelight code.
     * Use Constants file for target Height, limelight Height and mount
       Angle.
     * change distanceToTarget to use degrees (which is what limelight
       reports)
     * Set reasonable values (in feet) for mount height, mount Angle,
       and target Height.
     * Update LimelightSim to only refresh the Distance() and allow
       human updates to the other values.

--- src/test/java/frc/robot/subsystems/LimelightTest.java
+++ src/test/java/frc/robot/subsystems/LimelightTest.java
@@ -7,10 +7,6 @@ import frc.robot.subsystems.Limelight;
 public class LimelightTest {
   public static final double DELTA = 1e-2; // acceptable deviation range
 
-  public static double degToRad(double degrees) {
-      return degrees * Math.PI / 180;
-  }
-
   public static double feetToMeter(double ft) {
     final double MetersPerFoot = 0.3048;
     return ft * MetersPerFoot;
@@ -21,8 +17,8 @@ public class LimelightTest {
     double expected = 5.0f;
     double mH = 3.0f ; // camera is mounted around 3 feet high.
     double tH = 8.0f ; // target high hoop is 8 feet high.
-    double mountAngle = degToRad(20.0f) ; // say mounted at 20 degrees.
-    double offsetAngle = degToRad(25.0f) ; // as read from limelight.
+    double mountAngle = 20.0f ; // say mounted at 20 degrees.
+    double offsetAngle = 25.0f ; // as read from limelight.
     // tan(20 + 25) = 1, so this is just targetHeight - mountHeight
     Assert.assertEquals(
       expected, Limelight.distanceToTarget(mH, mountAngle, tH, offsetAngle), DELTA);

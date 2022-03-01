473419e15727a1363a1ad03f05ccd0f075fef43c
e-moser <78171469+e-moser@users.noreply.github.com>
2022-02-28 19:15:57 -0500

    Fixed stop turning bug

--- src/main/java/frc/robot/commands/Align.java
+++ src/main/java/frc/robot/commands/Align.java
@@ -44,7 +44,9 @@ public class Align extends CommandBase {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    m_drive.setDrive(0,0);
+  }
 
   // Returns true when the command should end.
   @Override

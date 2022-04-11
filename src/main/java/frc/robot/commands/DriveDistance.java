592b71d70156056344bbb135d3fa7db661528fc6
Scott Moser <smoser@brickies.net>
2022-04-11 18:03:00 +0000

    Attempt at some cleanup and DriveDistance.

--- src/main/java/frc/robot/commands/DriveDistance.java
+++ src/main/java/frc/robot/commands/DriveDistance.java
@@ -14,11 +14,9 @@ public class DriveDistance extends CommandBase {
   private final Drive m_drive;
   private Double m_distance;
   private Double lTarget, rTarget;
-  //Creating a margin of error for the motors
+  // Creating a margin of error for the motors
   private final double errorMargin = 0.2;
   private final double distancePerRotation = Constants.wheelDiameterFeet * Math.PI;
-  private boolean lDone = false;
-  private boolean rDone = false;
 
   // Distance is in inches.
   public DriveDistance(Drive subsystem, Double dist) {
@@ -34,12 +32,17 @@ public class DriveDistance extends CommandBase {
   public void initialize() {
       // This could use RelativeEncoder.setPositionConversionFactor to then use
       // a different unit.
-      double l = m_drive.getLeftRotations();
-      double r = m_drive.getRightRotations();
+      double curLeft = m_drive.getLeftRotations();
+      double curRight = m_drive.getRightRotations();
 
-      lTarget = m_drive.getLeftRotations() + (m_distance / distancePerRotation) + errorMargin;
-      rTarget = m_drive.getRightRotations() + (m_distance / distancePerRotation) + errorMargin;
+      lTarget = curLeft + (m_distance / distancePerRotation);
+      rTarget = curRight + (m_distance / distancePerRotation);
 
+      m_drive.setLeftTarget(lTarget);
+      m_drive.setRightTarget(rTarget);
+
+      SmartDashboard.putNumber("DLeftTar", lTarget);
+      SmartDashboard.putNumber("DRghtTar", rTarget);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
@@ -49,45 +52,24 @@ public class DriveDistance extends CommandBase {
     double r = m_drive.getRightRotations();
     SmartDashboard.putNumber("DLeftRot", l);
     SmartDashboard.putNumber("DRghtRot", r);
-    SmartDashboard.putNumber("DLeftTar", lTarget);
-    SmartDashboard.putNumber("DRghtTar", rTarget);
-  
-    if (!rDone) {
-      if (!isDone(rTarget, r, (m_distance < 0))) {
-        rDone = true;
-      } else {
-        m_drive.setRightRotations(rTarget);
-      }
-    }
-    if (!lDone) {
-      if (!isDone(lTarget, r, (m_distance < 0))) {
-        lDone = true;
-      } else {
-        m_drive.setRightRotations(lTarget);
-      }
-    }
   }
 
-  private boolean isDone(double target, double cur, boolean isPositive) {
-    if (isPositive) {
-        return (target + errorMargin >= cur);
-    }
-    return (target - errorMargin <= cur);
+  private static boolean isDone(double target, double cur, double closeEnough) {
+    return ((target - closeEnough) <= cur && cur <= (target + closeEnough));
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-      if (interrupted) {
-          // Since we were intereuppted, cancel the 'setPosition'
-          m_drive.setLeftRotations(m_drive.getLeftRotations());
-          m_drive.setRightRotations(m_drive.getRightRotations());
-      }
+      m_drive.cancelLeftTarget();
+      m_drive.cancelRightTarget();
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return (rDone && lDone);
+    double curLeft = m_drive.getLeftRotations();
+    double curRight = m_drive.getRightRotations();
+    return (isDone(lTarget, curLeft, errorMargin) && isDone(rTarget, curRight, errorMargin));
   }
 }
--- src/main/java/frc/robot/subsystems/Drive.java
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -21,21 +21,39 @@ public class Drive extends SubsystemBase {
 
 
     private CANSparkMax leftFront = new CANSparkMax(1, MotorType.kBrushless);
-    private CANSparkMax leftBack = new CANSparkMax(2, MotorType.kBrushless);
     private CANSparkMax rightFront = new CANSparkMax(4, MotorType.kBrushless);
+    private CANSparkMax leftBack = new CANSparkMax(2, MotorType.kBrushless);
     private CANSparkMax rightBack = new CANSparkMax(3, MotorType.kBrushless);
     private SparkMaxPIDController leftFrontPidController;
-    private SparkMaxPIDController leftBackPidController;
     private SparkMaxPIDController rightFrontPidController;
+    private SparkMaxPIDController leftBackPidController;
     private SparkMaxPIDController rightBackPidController;
-    private MotorControllerGroup leftGroup = new MotorControllerGroup(leftFront, leftBack);
-    private MotorControllerGroup rightGroup = new MotorControllerGroup(rightFront, rightBack);
     private RelativeEncoder leftFrontEncoder = leftFront.getEncoder();
-    private RelativeEncoder leftBackEncoder = leftBack.getEncoder();
     private RelativeEncoder rightFrontEncoder = rightFront.getEncoder();
+    private RelativeEncoder leftBackEncoder = leftBack.getEncoder();
     private RelativeEncoder rightBackEncoder = rightBack.getEncoder();
+    private boolean sparkFollow = true;
 
-    private DifferentialDrive driveDifferential = new DifferentialDrive(leftGroup, rightGroup);
+    private MotorControllerGroup leftGroup, rightGroup;
+    private DifferentialDrive driveDifferential;
+
+  public Drive() {
+    doInit();
+    if (sparkFollow) {
+      leftGroup = new MotorControllerGroup(leftFront);
+      rightGroup = new MotorControllerGroup(rightFront);
+    } else {
+      leftGroup = new MotorControllerGroup(leftFront, leftBack);
+      rightGroup = new MotorControllerGroup(rightFront, rightBack);
+    }
+
+    leftFrontPidController = leftFront.getPIDController();
+    rightFrontPidController = rightFront.getPIDController();
+    leftBackPidController = leftBack.getPIDController();
+    rightBackPidController = rightBack.getPIDController();
+
+    driveDifferential = new DifferentialDrive(leftGroup, rightGroup);
+  }
 
   public void arcadeDrive(DoubleSupplier speed, DoubleSupplier rotation) {
       driveDifferential.arcadeDrive(speed.getAsDouble() * 0.7f, rotation.getAsDouble() * -0.7f);
@@ -69,88 +87,98 @@ public class Drive extends SubsystemBase {
     return leftFrontEncoder.getPosition();
   }
 
-  public void setLeftRotations(double target) {
-    leftFrontPidController.setReference(target, CANSparkMax.ControlType.kPosition);
-  }
-
   public double getRightRotations() {
     return rightFrontEncoder.getPosition();
   }
 
-  public void setRightRotations(double target) {
+  public void cancelLeftTarget() {
+    // cancel the operation.  Another option is: leftFront.set(0);
+    leftFrontPidController.setReference(0, CANSparkMax.ControlType.kDutyCycle);
+    if (!sparkFollow) {
+      leftBackPidController.setReference(0, CANSparkMax.ControlType.kDutyCycle);
+    }
+  }
+
+  public void cancelRightTarget() {
+    // cancel the operation.  Another option is: rightFront.set(0);
+    rightFrontPidController.setReference(0, CANSparkMax.ControlType.kDutyCycle);
+    if (!sparkFollow) {
+      rightBackPidController.setReference(0, CANSparkMax.ControlType.kDutyCycle);
+    }
+  }
+
+  public void setLeftTarget(double target) {
+    leftFrontPidController.setReference(target, CANSparkMax.ControlType.kPosition);
+    if (!sparkFollow) {
+      leftBackPidController.setReference(target, CANSparkMax.ControlType.kPosition);
+    }
+  }
+
+  public void setRightTarget(double target) {
     rightFrontPidController.setReference(target, CANSparkMax.ControlType.kPosition);
+    if (!sparkFollow) {
+      rightBackPidController.setReference(target, CANSparkMax.ControlType.kPosition);
+    }
+  }
+
+  public static void setPID(SparkMaxPIDController pidc,
+        double p, double i, double d, double izone, double ff, double min, double max) {
+    pidc.setP(p);
+    pidc.setI(i);
+    pidc.setD(d);
+    pidc.setIZone(izone);
+    pidc.setFF(ff);
+    pidc.setOutputRange(min, max);
   }
 
   public void doInit() {
-
-    leftGroup.setInverted(true);
-
-    // PID coefficients
-    double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput;
-    kP = 0.1;
-    kI = 1e-4;
-    kD = 1;
-    kIz = 0;
-    kFF = 0;
-    kMaxOutput = 0.6;
-    kMinOutput = -0.6;
-
     /**
      * The restoreFactoryDefaults method can be used to reset the configuration parameters
      * in the SPARK MAX to their factory default state. If no argument is passed, these
      * parameters will not persist between power cycles
      */
     leftFront.restoreFactoryDefaults();
-    leftBack.restoreFactoryDefaults();
     rightFront.restoreFactoryDefaults();
+    leftBack.restoreFactoryDefaults();
     rightBack.restoreFactoryDefaults();
 
-    /**
-     * In order to use PID functionality for a controller, a SparkMaxPIDController object
-     * is constructed by calling the getPIDController() method on an existing
-     * CANSparkMax object
-     */
-    leftFrontPidController = leftFront.getPIDController();
-    leftBackPidController = leftBack.getPIDController();
-    rightFrontPidController = rightFront.getPIDController();
-    rightBackPidController = rightBack.getPIDController();
+    // zero rotations count for the encoder.
+    leftFrontEncoder.setPosition(0);
+    rightFrontEncoder.setPosition(0);
+    leftBackEncoder.setPosition(0);
+    rightBackEncoder.setPosition(0);
 
-     /**
+    /**
     * In CAN mode, one SPARK MAX can be configured to follow another. This is done by calling
     * the follow() method on the SPARK MAX you want to configure as a follower, and by passing
     * as a parameter the SPARK MAX you want to configure as a leader.
     */
-    leftBack.follow(leftFront);
-    rightBack.follow(rightFront);
+    if (sparkFollow) {
+      leftBack.follow(leftFront);
+      rightBack.follow(rightFront);
+    }
 
-    // set PID coefficients
-    leftFrontPidController.setP(kP);
-    leftFrontPidController.setI(kI);
-    leftFrontPidController.setD(kD);
-    leftFrontPidController.setIZone(kIz);
-    leftFrontPidController.setFF(kFF);
-    leftFrontPidController.setOutputRange(kMinOutput, kMaxOutput);
+    leftFront.setInverted(true);
+    if (!sparkFollow) {
+      leftBack.setInverted(true);
+    }
 
-    leftBackPidController.setP(kP);
-    leftBackPidController.setI(kI);
-    leftBackPidController.setD(kD);
-    leftBackPidController.setIZone(kIz);
-    leftBackPidController.setFF(kFF);
-    leftBackPidController.setOutputRange(kMinOutput, kMaxOutput);
+    // PID coefficients
+    double kP, kI, kD, kIz, kFF, kMin, kMax;
+    kP = 0.1;
+    kI = 1e-4;
+    kD = 1;
+    kIz = 0;
+    kFF = 0;
+    kMin = -0.6;
+    kMax = 0.6;
 
-    rightFrontPidController.setP(kP);
-    rightFrontPidController.setI(kI);
-    rightFrontPidController.setD(kD);
-    rightFrontPidController.setIZone(kIz);
-    rightFrontPidController.setFF(kFF);
-    rightFrontPidController.setOutputRange(kMinOutput, kMaxOutput);
-
-    rightBackPidController.setP(kP);
-    rightBackPidController.setI(kI);
-    rightBackPidController.setD(kD);
-    rightBackPidController.setIZone(kIz);
-    rightBackPidController.setFF(kFF);
-    rightBackPidController.setOutputRange(kMinOutput, kMaxOutput);
+    setPID(leftFront.getPIDController(), kP, kI, kD, kIz, kFF, kMax, kMin);
+    setPID(rightFront.getPIDController(), kP, kI, kD, kIz, kFF, kMax, kMin);
+    if (!sparkFollow) {
+      setPID(leftBack.getPIDController(), kP, kI, kD, kIz, kFF, kMax, kMin);
+      setPID(rightBack.getPIDController(), kP, kI, kD, kIz, kFF, kMax, kMin);
+    }
 
     // display PID coefficients on SmartDashboard
     // SmartDashboard.putNumber("P Gain", kP);
@@ -158,8 +186,8 @@ public class Drive extends SubsystemBase {
     // SmartDashboard.putNumber("D Gain", kD);
     // SmartDashboard.putNumber("I Zone", kIz);
     // SmartDashboard.putNumber("Feed Forward", kFF);
-    SmartDashboard.putNumber("DMax Output", kMaxOutput);
-    SmartDashboard.putNumber("DMin Output", kMinOutput);
+    SmartDashboard.putNumber("DMax Output", kMax);
+    SmartDashboard.putNumber("DMin Output", kMin);
     SmartDashboard.putNumber("Set Rotations", 0);
   }
 }
